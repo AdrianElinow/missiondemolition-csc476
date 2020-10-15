ed9323edbd91712124f969357710fd3bc8c44d9c
Adrian Elinow <45514532+AdrianElinow@users.noreply.github.com>
2020-10-15 15:08:32 -0400

    Added Slingshot and Projectile Objects
    Added Slingshot (+ prefab) and Projectile objects.

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchpoint;
+
+    void Awake(){
+        Transform launchpointTrans = transform.Find("LaunchPoint");
+        launchpoint = launchpointTrans.gameObject;
+        launchpoint.SetActive(false);
+    }
+
+    void onMouseEnter(){
+
+        print("Slingshot:OnMouseEnter()");
+        launchpoint.SetActive(true);
+
+    }
+
+    void onMouseExit(){
+
+        print("Slingshot:OnMouseExit()");
+        launchpoint.SetActive(false);
+    }
+
+
+}
