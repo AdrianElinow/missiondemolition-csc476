9dea31ae2c2f7d54a01665ec051430e70818b4cf
Adrian Elinow <45514532+AdrianElinow@users.noreply.github.com>
2020-10-19 20:27:14 -0400

    Completed Followcam
    Added physics material to projectile

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -6,6 +6,11 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // The static point of interest                // a
 
+     [Header("Set in Inspector")]
+
+    public float             easing = 0.05f;
+    public Vector2           minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
 
     public float             camZ; // The desired Z pos of the camera
@@ -26,10 +31,19 @@ public class FollowCam : MonoBehaviour
 
         // Get the position of the poi
         Vector3 destination = POI.transform.position;
+
+        destination.x = Mathf.Max( minXY.x, destination.x );
+
+        destination.y = Mathf.Max( minXY.y, destination.y );
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
         // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to the destination
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
 
     }
 }
